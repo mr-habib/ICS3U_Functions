5f8d6aea3e82cdcd590bc34155166afd89fdc096
Mr. Habib <78511534+mr-habib@users.noreply.github.com>
2021-03-03 21:22:28 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Firstname, L
+# mm/dd/yyyy
+# Descriptive Title
+
+def area_of_circle(r):
+  pass
+  
+def volume_of_sphere(r):
+  pass
+  
+def hypervolume_of_4D_sphere(r):
+  pass
+  
+def area_of_rect(l, w):
+  pass
+
+def volume_of_rect_prism(l, w, h):
+  pass
+
+def hypervolume_of_4D_prism(l, w, h, x):
+  pass
+  
+# Call your functions here to test them
