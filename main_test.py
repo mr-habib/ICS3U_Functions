1f203667e33d7738357b8652f7f6a52da824cbf6
Mr. Habib <sirmrhabib@gmail.com>
2021-05-17 12:56:45 -0400

    Modified for Q4 2021

--- main_test.py
+++ main_test.py
@@ -1,21 +1,20 @@
-
 from main import *
 
 def test_area_of_circle():
   assert area_of_circle(1) == 3.14
   assert area_of_circle(5) == 78.54
   assert area_of_circle(3.72) == 43.47
-  
+
 def test_volume_of_sphere():
   assert volume_of_sphere(1) == 4.19
   assert volume_of_sphere(57) == 775734.62
   assert volume_of_sphere(6.2832) == 1039.04
-  
+
 def test_hypervolume_of_4D_sphere():
   assert hypervolume_of_4D_sphere(1) == 4.93
   assert hypervolume_of_4D_sphere(4) == 1263.31
   assert hypervolume_of_4D_sphere(3.1415) == 480.64
-  
+
 def test_area_of_rect():
   assert area_of_rect(2, 3) == 6
   assert area_of_rect(32.56, 78.777) == 2564.98
